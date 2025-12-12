9c013bea983be5b432e536887525b8da30c4a231
Kira714 <kumbhatsachit@gmail.com>
2025-12-12 18:51:49 +0530

    Non imp files deleted

--- MCP_BATCH_FILE.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL -o LogLevel=ERROR -o ServerAliveInterval=60 -o ServerAliveCountMax=3 Sachit "/opt/airbyte-platform/airbyte-common-handler/backend/run_mcp_server.sh"
-
